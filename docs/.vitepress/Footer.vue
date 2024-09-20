508a1ad72fce65b8ec673c6b8d92c06eceac676d
felicia-haggqvist <felicia.haggqvist@schibsted.com>
2024-09-20 10:05:11 +0200

    fix(footer): fix dynamic width for footer

--- docs/.vitepress/Footer.vue
+++ docs/.vitepress/Footer.vue
@@ -29,6 +29,25 @@ const baseUrl = import.meta.env.BASE_URL;
   width: 100%;
   position: relative;
 }
+@media (min-width: 960px) {
+  .has-sidebar ~ .footer {
+  width: calc(100% - var(--vp-sidebar-width, 300px));
+  margin-left: var(--vp-sidebar-width, 300px);
+}
+}
+@media (min-width: 1440px) {
+  .has-sidebar ~ .footer {
+    width: calc(
+      100% - (
+        (100% - (var(--vp-layout-max-width) - 64px)) / 2 + var(--vp-sidebar-width) - 32px
+      )
+    );
+    margin-left: calc(
+      (100% - (var(--vp-layout-max-width) - 64px)) / 2 + var(--vp-sidebar-width) - 32px
+    ); 
+  }
+}
+
 .footer::before {
   content: '';
   position: absolute;
@@ -43,6 +62,7 @@ const baseUrl = import.meta.env.BASE_URL;
   z-index: 1;
   pointer-events: none;
 }
+
 .footer::after {
   content: '';
   position: absolute;
@@ -58,24 +78,13 @@ const baseUrl = import.meta.env.BASE_URL;
   z-index: 1;
   pointer-events: none;
 }
-@media (min-width: 960px){
-  .has-sidebar ~ .footer {
-    width: calc(100% - 264px);
-    margin-left: 264px;
-  }
-}
 
-@media (min-width: 1445px) {
-  .has-sidebar ~ .footer {
-    width: calc(100% - 504px);
-    margin-left: 504px;
-  }
-}
 .footer-column {
   flex: 1;
   margin: 0 20px;
   z-index: 2;
 }
+
 .footer-column-title {
   font-size: 14px;
   font-weight: 700;
@@ -83,6 +92,7 @@ const baseUrl = import.meta.env.BASE_URL;
   text-transform: uppercase;
   margin-bottom: 10px;
 }
+
 .footer-link {
   display: block;
   margin-bottom: 8px;
@@ -93,15 +103,17 @@ const baseUrl = import.meta.env.BASE_URL;
 .footer-link:hover {
   text-decoration: underline;
 }
+
 .logo-column {
   display: flex;
   justify-content: flex-start;
 }
 
-@media (max-width: 768px) {
+@media (max-width: 960px) {
   .footer {
     flex-direction: column;
     align-items: flex-start;
+    width: 100%;
   }
 
   .footer-column {
@@ -118,5 +130,4 @@ const baseUrl = import.meta.env.BASE_URL;
     font-size: 12px;
   }
 }
-
 </style>
