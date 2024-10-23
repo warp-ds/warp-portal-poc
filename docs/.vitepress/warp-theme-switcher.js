c26b6b7f5c568d6735ada02e20787d509f8b8368
Snorre Nygaard <snorre.nygaard@finn.no>
2024-10-23 12:47:26 +0200

    Add light suffix to the themeswitch light choices

--- docs/.vitepress/warp-theme-switcher.js
+++ docs/.vitepress/warp-theme-switcher.js
@@ -1,25 +1,35 @@
-import { computed, reactive } from "vue";
+import { computed, reactive } from 'vue';
 
 export default {
   install: (app) => {
     const themes = {
-      'DBA': 'dba-dk',
-      'Finn': 'finn-no',
-      'Tori': 'tori-fi'
+      'DBA light': 'dba-dk',
+      'Finn light': 'finn-no',
+      'Tori light': 'tori-fi',
     };
 
     const state = reactive({
-      currentTheme: (typeof window !== 'undefined' && localStorage.getItem('warpTheme')) || themes.Finn
+      currentTheme:
+        (typeof window !== 'undefined' &&
+          localStorage.getItem('warpTheme')) ||
+        themes.Finn,
     });
 
     const rewriteStylesheets = (theme) => {
-      const roots = [document, ...Array.from(document.querySelectorAll('*')).filter((el) => !!el.shadowRoot).map((el) => el.shadowRoot)];
+      const roots = [
+        document,
+        ...Array.from(document.querySelectorAll('*'))
+          .filter((el) => !!el.shadowRoot)
+          .map((el) => el.shadowRoot),
+      ];
       roots.forEach((root) => {
-        const stylesheets = root.querySelectorAll('link[rel="stylesheet"][href*="/@warp-ds/css/"]');
+        const stylesheets = root.querySelectorAll(
+          'link[rel="stylesheet"][href*="/@warp-ds/css/"]'
+        );
         stylesheets.forEach((s) => {
           s.setAttribute(
             'href',
-            `https://assets.finn.no/pkg/@warp-ds/css/v2/tokens/${theme}.css`,
+            `https://assets.finn.no/pkg/@warp-ds/css/v2/tokens/${theme}.css`
           );
         });
       });
@@ -27,14 +37,14 @@ export default {
 
     const updateTheme = (theme) => {
       localStorage.setItem('warpTheme', theme);
-      state.currentTheme = theme;  // Update the reactive state
+      state.currentTheme = theme; // Update the reactive state
       rewriteStylesheets(theme);
     };
 
     app.provide('warpThemeSwitcher', {
       themes,
       current: computed(() => state.currentTheme),
-      updateTheme
+      updateTheme,
     });
 
     // Listen for storage changes
@@ -46,5 +56,5 @@ export default {
         }
       });
     }
-  }
-}
+  },
+};
